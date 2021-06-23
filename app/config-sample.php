370a8d7bfaf8403873b75f18a07cff2921b8b4fd
Hela <hela.chatti.landolsi@gmail.com>
2021-06-23 15:28:44 +0200

    config

---
+++ app/config-sample.php
@@ -0,0 +1,12 @@
+<?php
+
+
+//informations de cnx à la bdd
+
+define('DB_HOST','');
+define('DB_NAME','');
+define('DB_USER','');
+define('DB_PASSWORD','');
+
+//url de base des liens
+define('SITE_BASE_URL','http://mon-site.com');
